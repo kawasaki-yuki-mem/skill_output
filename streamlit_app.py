0191b75279887715b225134b9a8193dc4c63b985
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-12 16:10:37 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,41 @@
+# import streamlit as st
+# import pandas as pd
+# import json
+# from snowflake.snowpark import Session
+
+# # ファイルアップロード
+# file = st.file_uploader("アクセスログをアップロードしてください。")
+
+# # データを閲覧するボタン
+# orgdata_view = st.button('データを閲覧する')
+
+# # cnx = st.connection("snowflake")
+# # session = cnx.session()
+
+# with open('creds.json') as f:
+#     connection_parameters = json.load(f)  
+# session = Session.builder.configs(connection_parameters).create()
+
+# # データ閲覧ボタンを押した場合
+# if orgdata_view:
+#   df = pd.read_csv(file)
+#   st.write(df)
+
+#   upload_button = st.button('アップロードする')
+  
+#   if upload_button:
+#     snowparkDf=session.write_pandas(df,file.name,auto_create_table = True, overwrite=True)
+
 import streamlit as st
 import pandas as pd
 import json
 from snowflake.snowpark import Session
 
-# ファイルアップロード
-file = st.file_uploader("アクセスログをアップロードしてください。")
-
-# データを閲覧するボタン
-orgdata_view = st.button('データを閲覧する')
-
-# cnx = st.connection("snowflake")
-# session = cnx.session()
-
+# connect to Snowflake
 with open('creds.json') as f:
     connection_parameters = json.load(f)  
 session = Session.builder.configs(connection_parameters).create()
 
-# データ閲覧ボタンを押した場合
-if orgdata_view:
-  df = pd.read_csv(file)
-  st.write(df)
-
-  upload_button = st.button('アップロードする')
-  
-  if upload_button:
-    snowparkDf=session.write_pandas(df,file.name,auto_create_table = True, overwrite=True)
+file = st.file_uploader("Drop your CSV here to load to Snowflake", type={"csv"})
+file_df = pd.read_csv(file)
+snowparkDf=session.write_pandas(file_df,file.name,auto_create_table = True, overwrite=True)
