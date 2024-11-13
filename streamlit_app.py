a6ff8650746928e1df444f30be3730075fa5073c
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-13 16:28:53 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,17 +109,17 @@ try:
   st.subheader("4. 各データの分布を確認")
 
   if uploaded_file is not None:
-    if etl_df is None:
-      st.subheader("4. 各データの分布を確認")
-      viz_edit = st.selectbox("選択してください", ['折れ線グラフ'])
-      x_list_edit = st.multiselect('x軸のカラムを選択してください'
-                                 , etl_df.columns)
-      y_list_edit = st.multiselect('y軸のカラムを選択してください'
-                                 , etl_df.columns)
+    # if etl_df is not None:
+    #   st.subheader("4. 各データの分布を確認")
+    #   viz_edit = st.selectbox("選択してください", ['折れ線グラフ'])
+    #   x_list_edit = st.multiselect('x軸のカラムを選択してください'
+    #                              , etl_df.columns)
+    #   y_list_edit = st.multiselect('y軸のカラムを選択してください'
+    #                              , etl_df.columns)
       
-      if viz_edit == '折れ線グラフ':
-        st.bar_chart(etl_df[x_list].select_dtypes(include='number'), etl_df[y_list].select_dtypes(include='number'))
-    elif etl_df is not None:
+    #   if viz_edit == '折れ線グラフ':
+    #     st.bar_chart(etl_df[x_list].select_dtypes(include='number'), etl_df[y_list].select_dtypes(include='number'))
+    if etl_df is None:
       viz_org = st.selectbox("選択してください", ['折れ線グラフ'])
       x_list_org = st.multiselect('x軸のカラムを選択してください'
                                    , df.columns)
