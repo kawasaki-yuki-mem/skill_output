dad1c3c65b00b67438b062944cf1681aa06b5c2e
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-13 10:45:25 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,31 +16,38 @@ file = st.sidebar.file_uploader("データファイルをアップロードし�
 if file is not None:
   # データの閲覧
   file_df = pd.read_csv(file)
-  st.write(file_df)
+  st.dataframe(file_df)
 
-  columns = st.selectbox("選択してください。", file_df.columns)
+  null_df = pd.DataFrame(df.isnull().sum(), columns=["null"])
+  st.dataframe(null_df)
 
-
-
-
-
-
-
-
-# Snowflakeにデータアップロードするボタン
-upload_button = st.sidebar.button('アップロードする')
-
-# データアップロードボタンを押した場合、Snowflakeにデータアップロード
-if upload_button:
+  null_df_all = pd.DataFrame(df.isnull(), columns=["null"])
+  st.dataframe(null_df_all)
   
-  # Snowflakeの資格情報を読み取る
-  with open('creds.json') as f:
-    connection_parameters = json.load(f)  
-  session = Session.builder.configs(connection_parameters).create()
+  
+  # columns = st.selectbox("選択してください。", file_df.columns)
 
-  # Snowflakeにデータアップロード
-  snowparkDf=session.write_pandas(df,file.name,auto_create_table = True, overwrite=True)
-  st.success('アップロード完了!', icon="✅")
+
+
+
+
+
+
+
+  # Snowflakeにデータアップロードするボタン
+  upload_button = st.sidebar.button('アップロードする')
+  
+  # データアップロードボタンを押した場合、Snowflakeにデータアップロード
+  if upload_button:
+    
+    # Snowflakeの資格情報を読み取る
+    with open('creds.json') as f:
+      connection_parameters = json.load(f)  
+    session = Session.builder.configs(connection_parameters).create()
+  
+    # Snowflakeにデータアップロード
+    snowparkDf=session.write_pandas(df,file.name,auto_create_table = True, overwrite=True)
+    st.success('アップロード完了!', icon="✅")
 
 # 参考
 # Snowflakeにデータアップロード　https://blog.streamlit.io/build-a-snowflake-data-loader-on-streamlit-in-only-5-minutes/
