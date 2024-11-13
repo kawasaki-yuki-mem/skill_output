23c91c28225f6e744779757ebfae73d3d5eaf366
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-13 12:15:44 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,19 +68,17 @@ try:
       elif mean_button:
         etl_df = df.fillna(df.mean(numeric_only=True))
         st.dataframe(etl_df)
-      else:
-      
-    st.sidebar.write("##")
-    st.sidebar.write("### ETL処理後")
-    st.sidebar.write(f"### サンプルサイズ:  {etl_df.shape[0]}")
-    st.sidebar.write(f"### カラム数     :  {etl_df.shape[1]}")
-    # 欠損値の表示
-    st.sidebar.write("### 欠損値")
-    st.sidebar.write("各カラムの欠損値")
-    null_etl_df = pd.DataFrame(etl_df.isnull().sum(), columns=["null"])
-    st.sidebar.dataframe(null_etl_df)
-    st.sidebar.write(f"### 合計欠損値数  :  {etl_df.isnull().sum().sum()}")
-    st.sidebar.write(f"### 重複行の数  :  {etl_df.duplicated().sum().sum()}")
+      st.sidebar.write("##")
+      st.sidebar.write("### ETL処理後")
+      st.sidebar.write(f"### サンプルサイズ:  {etl_df.shape[0]}")
+      st.sidebar.write(f"### カラム数     :  {etl_df.shape[1]}")
+      # 欠損値の表示
+      st.sidebar.write("### 欠損値")
+      st.sidebar.write("各カラムの欠損値")
+      null_etl_df = pd.DataFrame(etl_df.isnull().sum(), columns=["null"])
+      st.sidebar.dataframe(null_etl_df)
+      st.sidebar.write(f"### 合計欠損値数  :  {etl_df.isnull().sum().sum()}")
+      st.sidebar.write(f"### 重複行の数  :  {etl_df.duplicated().sum().sum()}")
       
   # 4. 各データの分布/割合を確認
   st.write("#")
