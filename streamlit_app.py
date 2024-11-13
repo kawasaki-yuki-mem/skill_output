4529a6a1889456e17b6bd18041bfcf41125bb16e
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-13 12:52:03 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -125,6 +125,12 @@ try:
       snowparkDf=session.write_pandas(df,file.name,auto_create_table = True, overwrite=True)
       st.success('アップロード完了!', icon="✅")
 
+except:
+    st.error(
+      """
+      エラーが発生しました。
+      """
+    )
 
 # 参考
 # Snowflakeにデータアップロード　https://blog.streamlit.io/build-a-snowflake-data-loader-on-streamlit-in-only-5-minutes/
