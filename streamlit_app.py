5e21d3e941717674f3b53ec74f2c36a260c16b9a
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-13 16:32:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,26 +108,26 @@ try:
   st.write("#")
   st.subheader("4. 各データの分布を確認")
 
-  if uploaded_file is not None:
-    # if etl_df is not None:
-    #   st.subheader("4. 各データの分布を確認")
-    #   viz_edit = st.selectbox("選択してください", ['折れ線グラフ'])
-    #   x_list_edit = st.multiselect('x軸のカラムを選択してください'
-    #                              , etl_df.columns)
-    #   y_list_edit = st.multiselect('y軸のカラムを選択してください'
-    #                              , etl_df.columns)
+  # if uploaded_file is not None:
+  #   # if etl_df is not None:
+  #   #   st.subheader("4. 各データの分布を確認")
+  #   #   viz_edit = st.selectbox("選択してください", ['折れ線グラフ'])
+  #   #   x_list_edit = st.multiselect('x軸のカラムを選択してください'
+  #   #                              , etl_df.columns)
+  #   #   y_list_edit = st.multiselect('y軸のカラムを選択してください'
+  #   #                              , etl_df.columns)
       
-    #   if viz_edit == '折れ線グラフ':
-    #     st.bar_chart(etl_df[x_list].select_dtypes(include='number'), etl_df[y_list].select_dtypes(include='number'))
-    if df is not None:
-      viz_org = st.selectbox("選択してください", ['折れ線グラフ'])
-      x_list_org = st.multiselect('x軸のカラムを選択してください'
-                                   , df.columns)
-      y_list_org = st.multiselect('y軸のカラムを選択してください'
-                                   , df.columns)
+  #   #   if viz_edit == '折れ線グラフ':
+  #   #     st.bar_chart(etl_df[x_list].select_dtypes(include='number'), etl_df[y_list].select_dtypes(include='number'))
+  #   if df is not None:
+  #     viz_org = st.selectbox("選択してください", ['折れ線グラフ'])
+  #     x_list_org = st.multiselect('x軸のカラムを選択してください'
+  #                                  , df.columns)
+  #     y_list_org = st.multiselect('y軸のカラムを選択してください'
+  #                                  , df.columns)
 
-      if viz_org == '折れ線グラフ':
-        st.bar_chart(df[x_list].select_dtypes(include='number'), df[y_list].select_dtypes(include='number'))
+  #     if viz_org == '折れ線グラフ':
+  #       st.bar_chart(df[x_list].select_dtypes(include='number'), df[y_list].select_dtypes(include='number'))
     
   # 5. Snowflakeにデータアップロード
   st.write("#")
