c07279db6477ab5b14fda558f8de88b5944b440e
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-13 10:21:39 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,22 +3,32 @@ import pandas as pd
 import json
 from snowflake.snowpark import Session
 
+# データアップロードの説明
 st.markdown(""" 
 ## データをテーブル表示
-- サイドバーの「Browse files」をクリックしてローカルにあるデータファイルを指定してアップロードしてください。  
+- 「Browse files」をクリックしてローカルにあるデータファイルを指定してアップロードしてください。  
 - 「データを閲覧する」をクリックしてアップロードされたデータファイルをテーブルで表示してください。
 """)
 
 # ファイルアップロード
 file = st.sidebar.file_uploader("データファイルをアップロードしてください。", type={"csv"})
+file_df = pd.read_csv(file)
 
 # データを閲覧するボタン
 orgdata_view = st.sidebar.button('データを閲覧する')
 
 # データ閲覧ボタンを押した場合、データ閲覧
 if orgdata_view:
-  file_df = pd.read_csv(file)
   st.write(file_df)
+
+
+
+
+
+
+
+
+
 
 # Snowflakeにデータアップロードするボタン
 upload_button = st.sidebar.button('アップロードする')
@@ -31,8 +41,8 @@ if upload_button:
     connection_parameters = json.load(f)  
   session = Session.builder.configs(connection_parameters).create()
 
-  # アップロードするデータ
-  file_df = pd.read_csv(file)
+  # # アップロードするデータ
+  # file_df = pd.read_csv(file)
 
   # Snowflakeにデータアップロード
   snowparkDf=session.write_pandas(file_df,file.name,auto_create_table = True, overwrite=True)
