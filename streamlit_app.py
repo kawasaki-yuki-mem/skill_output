e3005ffd99c563c374b0440bd4a87ddd29ee002d
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-14 16:44:38 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,7 +124,7 @@ try:
       elif viz_org == '棒グラフ':
         select_stack = st.selectbox('スタックを選択してください', [None, False, "layered", "normalize", "center"])
         select_horize = st.selectbox('水平に表示しますか', [False, True])
-        st.bar_chart(df[col_lst], x=st.selectbox('表示しますか', col_lst), y=st.selectbox('表示しますか', col_lst), horizontal=select_horize, stack=select_stack)
+        st.bar_chart(df[col_lst], x=st.selectbox('x表示しますか', col_lst), y=st.selectbox('y表示しますか', col_lst), horizontal=select_horize, stack=select_stack)
       
       elif viz_org == '散布図':
         st.scatter_chart(df[col_lst], x=col_lst, y=col_lst, )
