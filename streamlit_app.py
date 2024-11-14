1c1e0d8c5d2b608063a1b5d3c76a9a81e9dfaa3b
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-14 14:06:31 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import json
 from snowflake.snowpark import Session
-# import matplotlib.pyplot as plt
 
 st.write("#")
 
@@ -110,31 +109,42 @@ try:
   st.subheader("4. 各データの分布を確認")
 
   if uploaded_file is not None:
-    st.line_chart(df.select_dtypes(include='number'))
-    # fig, ax = plt.subplots()
-    # ax.hist(df, bins=20)
-    # st.pyplot(fig)
-    # if df.duplicated().sum().sum() == 0 and df.isnull().sum().sum() == 0:
-    #   viz_org = st.selectbox("選択してください", ['折れ線グラフ'])
-    #   x_list_org = st.multiselect('x軸のカラムを選択してください'
-    #                                , df.columns)
-    #   y_list_org = st.multiselect('y軸のカラムを選択してください'
-    #                                , df.columns)
+    # st.line_chart(df.select_dtypes(include='number'))
 
-    #   if viz_org == '折れ線グラフ':
-    #     st.bar_chart(df[x_list].select_dtypes(include='number'), df[y_list].select_dtypes(include='number'))
+  
+    if df.duplicated().sum().sum() == 0 and df.isnull().sum().sum() == 0:
+      viz_org = st.selectbox("選択してください", ['折れ線グラフ', '面グラフ', '棒グラフ', '散布図'])
+      x_list_org = st.multiselect('x軸のカラムを選択してください'
+                                   , df.columns)
+      y_list_org = st.multiselect('y軸のカラムを選択してください'
+                                   , df.columns)
+
+      if viz_org == '折れ線グラフ':
+        st.write(etl_df[x_list_edit])
+      #   st.bar_chart()
+      # elif viz_org == '面グラフ':
+      #   st.area_chart()
+      # elif viz_org == '棒グラフ':
+      #   st.bar_chart()
+      # elif viz_org == '散布図':
+      #   st.scatter_chart()
         
-    # if etl_df is not None:
-    #   st.subheader("4. 各データの分布を確認")
-    #   viz_edit = st.selectbox("選択してください", ['折れ線グラフ'])
-    #   x_list_edit = st.multiselect('x軸のカラムを選択してください'
-    #                              , etl_df.columns)
-    #   y_list_edit = st.multiselect('y軸のカラムを選択してください'
-    #                              , etl_df.columns)
+    if etl_df is not None:
+      viz_edit = st.selectbox("選択してください", ['折れ線グラフ', '面グラフ', '棒グラフ', '散布図']])
+      x_list_edit = st.multiselect('x軸のカラムを選択してください'
+                                 , etl_df.columns)
+      y_list_edit = st.multiselect('y軸のカラムを選択してください'
+                                 , etl_df.columns)
       
-    #   if viz_edit == '折れ線グラフ':
-    #     # st.write(etl_df[x_list_edit])
-    #     st.bar_chart(etl_df[x_list_edit], etl_df[y_list_edit])
+      if viz_edit == '折れ線グラフ':
+        # st.write(etl_df[x_list_edit])
+      #   st.bar_chart(etl_df[x_list_edit], etl_df[y_list_edit])
+      # elif viz_edit == '面グラフ':
+      #   st.area_chart()
+      # elif viz_edit == '棒グラフ':
+      #   st.bar_chart()
+      # elif viz_edit == '散布図':
+      #   st.scatter_chart()
     
   # 5. Snowflakeにデータアップロード
   st.write("#")
