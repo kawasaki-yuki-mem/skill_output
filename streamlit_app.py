01a82e2d424498e562e3007b235211c82adb677f
kawasaki-yuki-mem <kawasaki.yuki.4927@members.co.jp>
2024-11-14 15:26:28 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -124,8 +124,8 @@ try:
         
       elif viz_org == '面グラフ':
         col_lst = st.multiselect('カラムを選択してください', df.columns)
-        select_stack = st.selectbox('スタックを選択してください', ["None", "True", "False", "normalize", "center"])
-        st.area_chart(df[col_lst], stack="None")
+        select_stack = st.selectbox('スタックを選択してください', [None, True, False, "normalize", "center"])
+        st.area_chart(df[col_lst], stack=select_stack)
         
       elif viz_org == '棒グラフ':
         st.bar_chart()
